133bda5c98589e94ad70e393fc3a8539c6c698b4
Yohanes Debebe <yopeman318@gmail.com>
2025-08-13 09:28:38 +0300

    build the react app with some modification of directory structure.

--- Express/app.js
+++ Express/app.js
@@ -10,7 +10,12 @@ import { sequelize } from './models/index.js';
 import request_parser from './utilities/requests.js';
 import isAuth from './utilities/auths.js';
 import { payment_webhook } from './controllers/payments.js';
-import {get_my_certificate_doc, redirect_certification_doc} from "./controllers/certifications.js";
+import { get_my_certificate_doc, redirect_certification_doc } from './controllers/certifications.js';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(cors());
@@ -18,17 +23,49 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(request_parser);
-app.get('/api', (req, res) => { res.json('AiPLP server is lived...') })
+
+// Health check route
+app.get('/api', (req, res) => {
+    res.json('AiPLP server is live...');
+});
+
+// Authentication routes
 app.use(auth_routes);
+
+// Payment webhook and certification routes
 app.use('/payments/webhook', payment_webhook);
 app.use('/certifications/field/:fieldId/user/:userId/doc', get_my_certificate_doc);
 app.use('/results/:resultId/link', redirect_certification_doc);
+
+// API routes with authentication
 app.use('/api/v1/', isAuth, routes);
+
+// Serve static files for the admin panel
+app.use('/admin', express.static(path.join(__dirname, 'static', 'admin-dashboard')));
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'admin-dashboard', 'index.html'));
+});
+
+// Serve static files for the assistant panel
+app.use('/assistant', express.static(path.join(__dirname, 'static', 'assistant-dashboard')));
+app.get('/assistant', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'assistant-dashboard', 'index.html'));
+});
+
+// // Serve static files for the landing page
+app.use(express.static(path.join(__dirname, 'static', 'landing-page')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'landing-page', 'index.html'));
+});
+
+// Error handling middleware
 app.use(handler);
 app.use(error_handler);
-sequelize.sync().then().catch();
 
-app.listen(process.env.PORT, (e) => {
-    if (e) console.error(e);
-    console.log(`http://127.0.0.1:${process.env.PORT}/api`);
-});
+// Sync the database and start the server
+sequelize.sync().then(() => {
+    app.listen(process.env.PORT, (err) => {
+        if (err) console.error(err);
+        console.log(`Server running at http://127.0.0.1:${process.env.PORT}`);
+    });
+}).catch(err => console.error(err));
