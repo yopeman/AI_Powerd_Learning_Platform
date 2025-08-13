97a7b42c9d7cf1058399c34768d0564c0b58d2f7
Yohanes Debebe <yopeman318@gmail.com>
2025-08-13 20:44:41 +0300

    change the database from mysql to postgress

--- models/config.js
+++ models/config.js
@@ -8,7 +8,8 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
     logging: false
 });
 
